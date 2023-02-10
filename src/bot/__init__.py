a9e527c6981d63227a924a0db32d4802736cd16f
te <thrizzle@skiff.com>
2023-02-10 18:16:18 -0500

    🐛 try to fix statcord again

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,34 +1,33 @@
 from discord import Intents, Game, MemberCacheFlags
-from discord.ext.bridge.bot import Bot
-from discord.ext.commands import when_mentioned
+from discord.ext.commands import when_mentioned, Bot
 from src.views import CreateTicket, TicketSettings
 import aiosqlite
 from aiofiles import open as aopen
 from statcord import StatcordClient
+from typing import Optional, List, Tuple
 
 
 class PurpBot(Bot):
-    def __init__(self, statcord_key: str = None, *args, **kwargs):
+    def __init__(self, statcord_key: Optional[str], *args, **kwargs):
         intents = Intents.default()
         # intents.members = True
         intents.message_content = True
         self.statcord_key = statcord_key
-        self.reaction_roles = []
+        self.reaction_roles: List[Tuple[int, int, int]] = []
         super().__init__(
+            command_prefix=when_mentioned,
             intents=intents,
             test_guilds=[1050102412104437801],
-            command_prefix=when_mentioned,
             member_cache_flags=MemberCacheFlags.none(),
             max_messages=None,
             *args,
             **kwargs,
         )
+        self.statcord = StatcordClient(self, self.statcord_key)
 
     async def on_ready(self):
         print("PurpBot is online!")
         await self.change_presence(activity=Game("/info"))
-        if self.statcord_key:
-            self.statcord = StatcordClient(self, self.statcord_key)
         self.add_view(CreateTicket())
         self.add_view(TicketSettings())
 
