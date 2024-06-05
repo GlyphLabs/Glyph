46f8cd8a3d0de3c51e78989e1dd51bfb925e5f8d
te <thrzl@icloud.com>
2024-06-04 22:24:05 -0400

    🚸  all around improve mod cog

--- src/cogs/moderation.py
+++ src/cogs/moderation.py
@@ -1,3 +1,4 @@
+from discord.commands import SlashCommandGroup, option
 from src.bot import Glyph
 from discord.ext.commands import (
     slash_command,
@@ -6,7 +7,7 @@ from discord.ext.commands import (
     cooldown,
     BucketType,
 )
-from discord import Embed, Option, ApplicationContext, Member, Colour, Role
+from discord import Embed, ApplicationContext, Member, Colour, Role
 from discord.ext.commands.errors import MissingPermissions, CommandOnCooldown
 from datetime import timedelta
 from humanize import naturaldelta
@@ -21,15 +22,13 @@ class Moderation(Cog):
 
     @slash_command(name="kick", description="Kicks a member | /kick [member]")
     @has_permissions(kick_members=True)
+    @option(name="member", description="The member you want to kick", type=Member, required=True)
+    @option(name="reason", description="The reason to kick the member", type=str, required=True)
     async def kick(
         self,
-        ctx,
-        member: Option(
-            Member, description="The member you want to kick", required=True
-        ),
-        reason: Option(
-            str, description="The reason the member is kicked", required=False
-        ),
+        ctx: ApplicationContext,
+        member: Member,
+        reason: str,
     ):
         if member.guild_permissions.administrator:
             em = Embed(
@@ -56,7 +55,7 @@ class Moderation(Cog):
             await ctx.respond(embed=embed)
 
     @kick.error
-    async def kickerror(self, ctx, error):
+    async def kickerror(self, ctx: ApplicationContext, error):
         if isinstance(error, MissingPermissions):
             embed = Embed(
                 description="You are missing the `KICK_MEMBERS` permission",
@@ -81,13 +80,13 @@ class Moderation(Cog):
 
     @slash_command(name="ban", description="Bans a member | /ban [member]")
     @has_permissions(ban_members=True)
+    @option(name="member", description="The member you want to ban", type=Member, required=True)
+    @option(name="reason", description="The reason to ban the member", type=str, required=True)
     async def ban(
         self,
-        ctx,
-        member: Option(Member, description="The member you want to ban", required=True),
-        reason: Option(
-            str, description="The reason the member is banned", required=False
-        ),
+        ctx: ApplicationContext,
+        member: Member,
+        reason: str,
     ):
         if member.guild_permissions.administrator:
             em = Embed(
@@ -114,7 +113,7 @@ class Moderation(Cog):
             await ctx.respond(embed=embed)
 
     @ban.error
-    async def banerror(self, ctx, error):
+    async def banerror(self, ctx: ApplicationContext, error):
         if isinstance(error, MissingPermissions):
             embed = Embed(
                 description="You are missing the `BAN_MEMBERS` permission",
@@ -139,17 +138,13 @@ class Moderation(Cog):
 
     @slash_command(name="warn", description="Warns someone | /warn [member] <reason>")
     @has_permissions(manage_messages=True)
+    @option(name="member", description="The member you want to warn", type=Member, required=True)
+    @option(name="reason", description="The reason to warn the member", type=str, required=True)
     async def warn(
         self,
         ctx: ApplicationContext,
-        member: Option(
-            Member, description="The member you want to warn", required=True
-        ),
-        reason: Option(
-            str="No reason specified",
-            description="The reason for the warn",
-            required=False,
-        ),
+        member: Member,
+        reason: str,
     ):
         await self.addwarn(ctx, reason, member)
         embed = Embed(
@@ -160,25 +155,19 @@ class Moderation(Cog):
             name="Success",
             icon_url="https://cdn.discordapp.com/emojis/1055805763651641355.webp?size=96&quality=lossless",
         )
-        await ctx.respond(embed=embed)
+        await ctx.respond(embed=embed, ephemeral=True)
 
     @slash_command(
         name="unwarn", description="Delete all of someone's warn(s) | /unwarn [member]"
     )
     @has_permissions(manage_guild=True)
+    @option(name="member", description="The member you want to remove warns from", type=Member, required=True)
     async def unwarn(
         self,
         ctx: ApplicationContext,
-        member: Option(
-            Member,
-            description="The member you want to remove the warns from",
-            required=True,
-        ),
+        member: Member,
     ):
-        await self.bot.conn.execute(
-            "DELETE FROM warns WHERE user = ? AND guild = ?",
-            (member.id, ctx.guild.id),
-        )
+        await self.bot.db.delete_warns(member.id, ctx.guild.id)
         embed = Embed(
             colour=Colour.green(),
             description=f"Removed all of {member.mention}'s warn(s)",
@@ -192,10 +181,11 @@ class Moderation(Cog):
     @slash_command(
         name="warns", description="Shows someone's warnings | /warrns [member]"
     )
+    @option(name="member", description="The member you want to remove warns from", type=Member, required=True)
     async def warns(
         self,
         ctx: ApplicationContext,
-        member: Option(Member, description="The member's warns", required=True),
+        member: Member,
     ):
         rows = await self.bot.db.get_warns(member.id, ctx.guild.id)
         if rows:
@@ -214,11 +204,13 @@ class Moderation(Cog):
             await ctx.respond(embed=embed)
 
     @slash_command(name="timeout", description="Puts a member in timeout")
+    @option(name="member", description="The member you want to time out", type=Member, required=True)
+    @option(name="time", description="The period of time to time the user out for. Expressed as 1d, 5, etc.", type=str, required=True)
     async def timeout(
         self,
-        ctx,
-        member: Option(Member),
-        time: Option(str, description="A period of time, expressed as 1d, 5m, etc."),
+        ctx: ApplicationContext,
+        member: Member,
+        time: str
     ):
         """Apply a timeout to a member"""
         time_units = {
@@ -231,7 +223,7 @@ class Moderation(Cog):
         try:
             seconds = int(time[:-1]) * time_units[(time[-1])]
         except Exception:
-            await ctx.respond(f"Invalid time `{time}`")
+            return await ctx.respond(f"Invalid time `{time}`")
 
         duration = timedelta(seconds=seconds)
         # timeout for the amount of time given, then remove timeout
@@ -241,21 +233,21 @@ class Moderation(Cog):
     @slash_command(name="purge", description="Delete a certain amount of messages")
     @has_permissions(manage_messages=True)
     @cooldown(1, 5, BucketType.user)
+    @option(name="messages", description="The amount of messages you want to delete", type=int, required=True)
     async def purge(
         self,
-        ctx,
-        messages: Option(
-            int, description="The amount of messages you want to delete", required=True
-        ),
+        ctx: ApplicationContext,
+        messages: int
     ):
         await ctx.defer()
-        x = await ctx.channel.purge(limit=messages)
-        await ctx.respond(f"Deleted {len(x)}", ephemeral=True)
+        m = await ctx.respond(f"Deleting {messages}...", ephemeral=True)
+        x = await ctx.channel.purge(limit=messages) # type: ignore
+        await m.edit(content=f"Deleted {len(x)} messages")
 
     # error handler
 
     @purge.error
-    async def purgeerror(self, ctx, error):
+    async def purgeerror(self, ctx: ApplicationContext, error):
         if isinstance(error, MissingPermissions):
             embed = Embed(
                 description="You are missing the `MANAGE_MESSAGES` permission",
@@ -271,13 +263,17 @@ class Moderation(Cog):
         else:
             raise error
 
-    @slash_command(name="addrole", description="Gives a user a role")
+    role = SlashCommandGroup("role", "Manage a user's roles")
+
+    @role.command(name="add", description="Gives a user a role")
     @has_permissions(manage_guild=True)
+    @option(name="role", description="The role to give the member", type=Role, required=True)
+    @option(name="member", description="The member you want to give the role to", type=Member, required=True)
     async def addrole(
         self,
         ctx: ApplicationContext,
-        role: Option(Role, description="The role you want to add", required=True),
-        member: Option(Member, description="The member who'll get the role"),
+        role: Role,
+        member: Member,
     ):
         await member.add_roles(role)
         embed = Embed(
@@ -315,13 +311,15 @@ class Moderation(Cog):
             await ctx.respond(embed=em, ephemeral=True)
             raise error
 
-    @slash_command(name="removerole", description="Removes from a user a role")
+    @slash_command(name="remove", description="Removes a role from a user")
     @has_permissions(manage_guild=True)
+    @option(name="role", description="The role to give the member", type=Role, required=True)
+    @option(name="member", description="The member you want to give the role to", type=Member, required=True)
     async def removerole(
         self,
         ctx: ApplicationContext,
-        role: Option(Role, description="The role you want to remove", required=True),
-        member: Option(Member, description="The member"),
+        role: Role,
+        member: Member,
     ):
         await member.add_roles(role)
         embed = Embed(
