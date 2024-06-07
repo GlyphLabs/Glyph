913132e343ed75909bf5ea4cab22120048fc25b8
te <thrzl@icloud.com>
2024-06-06 20:35:39 -0400

    ✨ make bot aware of version

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from src.bot import Glyph
 from dotenv import load_dotenv
 from os import environ
+from tomllib import loads
+
+with open("pyproject.toml") as f:
+    pyproject = loads(str(f.read()))
 
 load_dotenv()
 
 bot = Glyph(
     database_url=environ.get("DATABASE_URL"),
     test_mode=bool(int(environ.get("TEST_MODE", 0))),
+    emoji_guild=int(environ.get("EMOJI_GUILD")) if environ.get("EMOJI_GUILD") else None,
+    version=pyproject["project"]["version"],
 )
 bot.remove_command("help")
 
